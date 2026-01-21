ac06183675dc733b5fa6bb71afa3f29900af7dcc
shankarp369 <shankar310105@gmail.com>
2026-01-21 12:28:48 +0530

    Day 1 challenge

---
+++ day1_hello_genai.py
@@ -0,0 +1,9 @@
+# Python 3.10+ Installed
+# version - Python 3.10.19
+
+# Virtual Environment Created in minicoda
+# conda activate langchain
+
+# First Script hello_genai.py
+
+print("hello_genai")
